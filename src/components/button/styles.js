a0f5e81edd3216a92985ad7f722f9ca9be18a2bd
Marcus Trento <marcusvinicius_ct@hotmail.com>
2023-02-03 22:13:22 -0300

    first commit

---
+++ src/components/button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 14px;
+    border: 1px solid #CDCDCD;
+    background-color: #028090;
+    color: #E7F6F2;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/input/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #003459;
+        border: 0;
+        padding: 0 10px;
+
+        text-align: right;
+        font-size: 38px;
+        font-family: 'Roboto';
+        color: #FCE38A;
+    }
+`
